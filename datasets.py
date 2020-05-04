1d50e29a99ad116334e7685e51a3600e3b081789
Susheel Varma <susheel.varma@gmail.com>
2020-05-04 17:06:26 +0100

    Added field to note if the dataset has a data class (technical metadata)

--- datasets.py
+++ datasets.py
@@ -14,6 +14,7 @@ from pprint import pprint
 API_BASE_URL="https://metadata-catalogue.org/hdruk/api"
 DATA_MODELS = API_BASE_URL + "/dataModels"
 DATA_MODEL_ID = API_BASE_URL + "/facets/{ID}/profile/uk.ac.hdrukgateway/HdrUkProfilePluginService"
+DATA_MODEL_CLASSES = DATA_MODELS + "/{ID}/dataClasses?offset=0&max=20"
 
 def request_url(URL):
   """HTTP GET request and load into json"""
@@ -31,6 +32,7 @@ def export_csv(data, filename, header=None):
     writer.writerows(data)
 
 def process_data_models(data_models):
+  print("Processing Data Models")
   count = data_models['count']
   headers = []
   data = []
@@ -42,9 +44,22 @@ def process_data_models(data_models):
   print("Retrieved ", count, " records.")
   return data, list(set(headers))
 
+def process_data_classes(data_models, headers):
+  print("Processing Data Classes")
+  data = []
+  headers.append('has_data_classes')
+  for d in data_models:
+    URL = DATA_MODEL_CLASSES.format(ID=d['id'])
+    row = request_url(URL)
+    data_model_count = int(row.get('count', 0))
+    d['has_data_classes'] = data_model_count > 0
+    data.append(d)
+  return data, headers
+
 def main():
   data_models = request_url(DATA_MODELS)
   data, headers = process_data_models(data_models)
+  data, headers = process_data_classes(data, headers)
   export_csv(data, 'datasets.csv', headers)
 
 if __name__ == "__main__":
