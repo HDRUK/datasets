732cb2f957a725fa2ab1316b6c1d203ae80a8ac8
Susheel Varma <susheel.varma@gmail.com>
2020-05-15 06:52:48 +0100

    Summarising quality scores

--- quality_checks.py
+++ quality_checks.py
@@ -6,6 +6,8 @@ __email__ = "susheel.varma@hdruk.ac.uk"
 __license__ = "Apache 2"
 
 import copy
+import math
+from statistics import mean
 import csv
 import json
 import urllib
@@ -15,7 +17,7 @@ from validate_schema import get_json, validate_schema
 from datasets import export_csv, export_json
 
 DATASET_SCHEMA = 'https://raw.githubusercontent.com/HDRUK/schemata/master/schema/dataset.schema.json'
-BASELINE_SCHEMA = 'https://raw.githubusercontent.com/HDRUK/schemata/master/examples/dataset.sample.json'
+BASELINE_SAMPLE = 'https://raw.githubusercontent.com/HDRUK/schemata/master/examples/dataset.sample.json'
 DATASETS_JSON = "datasets.json"
 
 METADATA_SECTIONS = {
@@ -63,7 +65,7 @@ def nullScore(d):
     return data
 
 def completeness_check():
-    schema = get_json(BASELINE_SCHEMA)
+    schema = get_json(BASELINE_SAMPLE)
     data_models = get_json(DATASETS_JSON)
     data = []
     header = []
@@ -101,14 +103,53 @@ def schema_validation_check():
         data.append(d)
     return data, list(set(headers))
 
+def generate_quality_score():
+    # TODO: Differentiate between section (A-G) completeness by weighting them
+    scores = get_json('reports/completeness.json')
+    data = {}
+    for s in scores:
+        data[s['id']] = {
+            'id': s['id'],
+            'publisher': s['publisher'],
+            'title': s['title']
+        }
+        c_score = round((s['missing_attributes'] / s['total_attributes']) * 100, 2)
+        data[s['id']]['completeness_percent'] = c_score
+    
+    # TODO: Differentiate between error classes (required vs format) by weighting them
+    schema = get_json(DATASET_SCHEMA)
+    total_attributes = len(list(schema['properties'].keys()))
+    errors = get_json('reports/schema_errors.json')
+    for e in errors:
+        e_score = round((e['schema_error_count'] / total_attributes) * 100, 2)
+        data[e['id']]['error_percent'] = e_score
+    
+    # Calculate average quality score (lower the better)
+    summary_data = []
+    headers = []
+    for id, d in data.items():
+        avg_score = round(mean([data[id]['completeness_percent'], data[id]['error_percent']]), 2)
+        d['quality_score'] = avg_score
+        headers.extend(d.keys())
+        summary_data.append(d)
+    return summary_data, list(set(headers))
+
 
 def main():
+    # Compile Metadata Completeness Score
     completeness_score, headers = completeness_check()
     export_json(completeness_score,'reports/completeness.json')
     export_csv(completeness_score, 'reports/completeness.csv', headers)
+
+    # Complie Schema Validation Error Score
     schema_errors, headers = schema_validation_check()
     export_json(schema_errors,'reports/schema_errors.json')
     export_csv(schema_errors, 'reports/schema_errors.csv', headers)
+
+    # Summarise Average Quality Score
+    summary_score, headers = generate_quality_score()
+    export_json(summary_score,'reports/dataset_quality.json')
+    export_csv(summary_score, 'reports/dataset_quality.csv', headers)
 
 
 if __name__ == "__main__":
