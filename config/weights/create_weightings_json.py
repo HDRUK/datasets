71edb149460486eaff79d7a3a71def7fa9b140da
Courtney Irwin <courtneyirwin@Courtneys-MacBook-Pro.local>
2020-05-28 10:44:58 +0100

    small bug in create_weightings_json

--- config/weights/create_weightings_json.py
+++ config/weights/create_weightings_json.py
@@ -5,20 +5,20 @@ REPORTING_LEVELS = ["A: Summary", "B: Business", "C: Coverage & Detail",
                     "D: Format & Structure", "E: Attribution", "F: Technical Metadata"]
 
 def main():
-    df = pd.read_csv("weightings_config.tsv", sep="\t")
+    df = pd.read_csv("config/weights/weightings_config.tsv", sep="\t")
     main_dict = {}
     for section in REPORTING_LEVELS:
         section_df = df[df["Section"] == section]
         section_df = section_df[["Attribute", "Weighting"]]
         sub_dict = {}
         for r, v in section_df.iterrows():
-            sub_dict[v["Attribute"]] = v["Weighting"]
+            sub_dict[v["Attribute"]] = float(v["Weighting"])
         main_dict[section] = sub_dict
     # Serializing json
     json_object = json.dumps(main_dict, indent=4)
 
     # Writing to sample.json
-    with open("weights.json", "w") as outfile:
+    with open("config/weights/weights.json", "w") as outfile:
         outfile.write(json_object)
 
 
