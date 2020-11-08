64b615d413bd7d5ac5f30405038560304daf53d7
Susheel Varma <susheel.varma@gmail.com>
2020-11-08 13:55:50 +0000

    Creating v1 avd v2 metadata quality weightings

--- config/weights/create_weightings_json.py
+++ config/weights/create_weightings_json.py
@@ -1,26 +1,32 @@
-import pandas as pd
+import sys
 import json
+import pandas as pd
 
-REPORTING_LEVELS = ["A: Summary", "B: Business", "C: Coverage & Detail",
-                    "D: Format & Structure", "E: Attribution", "F: Technical Metadata"]
-
-def main():
-    df = pd.read_csv("config/weights/weightings_config.tsv", sep="\t")
-    main_dict = {}
-    for section in REPORTING_LEVELS:
+def main(in_file, out_file):
+    df = pd.read_csv(in_file)
+    df['Weighting'] = pd.to_numeric(df['Weighting'], errors='coerce')
+    data = {}
+    sections = df['Section'].unique()
+    for section in sections:
         section_df = df[df["Section"] == section]
-        section_df = section_df[["Attribute", "Weighting"]]
-        sub_dict = {}
+        data[section] = {}
         for r, v in section_df.iterrows():
-            sub_dict[v["Attribute"]] = float(v["Weighting"])
-        main_dict[section] = sub_dict
-    # Serializing json
-    json_object = json.dumps(main_dict, indent=4)
+            data[section][v["Attribute"]] = v["Weighting"]
 
     # Writing to sample.json
-    with open("config/weights/weights.json", "w") as outfile:
-        outfile.write(json_object)
+    with open(out_file, "w") as outfile:
+        outfile.write(json.dumps(data, indent=4))
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description='Process metadata quality weights into JSON')
+    parser.add_argument('--in', metavar='CSV', dest='in_file', type=str, help='Input CSV path to metadata quality weights')
+    parser.add_argument('--out', metavar='JSON', dest='out_file', type=str, help='Output JSON path for metadata quality weights')
+
+    args = parser.parse_args()
+    if args.in_file is None and args.out_file is None:
+        parser.print_help()
+        sys.exit(1)
+
+    main(args.in_file, args.out_file)
